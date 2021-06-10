b09b2853d2c8c60010e8ef1865bd645cec75ce60
Fabiana Ferreira <fabiana.caroline13@gmail.com>
2021-06-10 20:08:39 -0300

    Exercício classe 05 - 10-06

--- aula-10-06/back-integral-variaveis-main/classe-05/index.js
+++ aula-10-06/back-integral-variaveis-main/classe-05/index.js
@@ -1,3 +1,9 @@
-console.log("Resolvido 2");
+let distancia = 0;
+const x1 = 1;
+const x2 = 1;
+const y1 = 1;
+const y2 = 4;
 
+distancia = Math.sqrt((Math.pow((x2-x1), 2)) + (Math.pow((y2-y1), 2)));
 
+console.log(`A distância entre os dois pontos é ${distancia}`);
