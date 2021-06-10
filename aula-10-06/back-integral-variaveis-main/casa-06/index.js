abf8211269fbb1472048fba1b50160ac29520062
Fabiana Ferreira <fabiana.caroline13@gmail.com>
2021-06-10 20:19:16 -0300

    Exercício casa 06 - 10-06

---
+++ aula-10-06/back-integral-variaveis-main/casa-06/index.js
@@ -0,0 +1,7 @@
+let velocidadeMedia = 0;
+const distancia = 9;
+const tempo = 30;
+
+velocidadeMedia = distancia/tempo;
+
+console.log(`A velocidade média é de ${(velocidadeMedia*3.6).toFixed(2)} Km/h`);
