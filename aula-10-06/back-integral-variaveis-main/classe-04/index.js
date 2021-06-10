615de06cea352e25d9e0c8aedb64a99bb125cdb5
Fabiana Ferreira <fabiana.caroline13@gmail.com>
2021-06-10 19:57:58 -0300

    Exercício classe 04 - 10-06

---
+++ aula-10-06/back-integral-variaveis-main/classe-04/index.js
@@ -0,0 +1,8 @@
+let montante = 0;
+const capital = 1000;
+const taxa = 12.5;
+const tempo = 5;
+
+montante = capital * Math.pow((1 + (taxa/100)), tempo);
+
+console.log(`O valor do montante é: ${Math.round(montante)}`);
