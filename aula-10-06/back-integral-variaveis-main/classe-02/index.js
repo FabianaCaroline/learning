50575004a466e39414d9235588c09229fa8a9e95
Fabiana Ferreira <fabiana.caroline13@gmail.com>
2021-06-10 19:38:40 -0300

    Exercício classe 02 - 10-06

---
+++ aula-10-06/back-integral-variaveis-main/classe-02/index.js
@@ -0,0 +1,6 @@
+const fahrenheit = 73;
+let celcius = 0;
+
+celcius = (fahrenheit-32)*(5/9);
+
+console.log(` A conversão de ${fahrenheit}° fahrenheit é igual a` + Math.round(celcius) + "° celcius.");
