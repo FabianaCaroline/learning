828e32c5df065fe10e7a49945c9f88bb07f3a1c8
Fabiana Ferreira <fabiana.caroline13@gmail.com>
2021-06-10 21:50:23 -0300

    Exercício extra 11 - 10-06

---
+++ aula-10-06/back-integral-variaveis-main/extra-11/index.js
@@ -0,0 +1,8 @@
+let volume = 0;
+let raio = 0;
+const diametro = 6;
+
+raio = diametro/2;
+volume = (4/3)*Math.pow(raio, 3);
+
+console.log(`O volume de uma esfera de raio ${raio} é igual a ${volume} PI`);
