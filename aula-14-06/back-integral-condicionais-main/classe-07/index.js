eb53c89f5ed0994aef6a953738101b03d69a0e85
Fabiana Ferreira <fabiana.caroline13@gmail.com>
2021-06-14 19:34:34 -0300

    :tada: Exercício 7 - 14-06

--- aula-14-06/back-integral-condicionais-main/classe-07/index.js
+++ aula-14-06/back-integral-condicionais-main/classe-07/index.js
@@ -2,4 +2,12 @@ const aposentada = false;
 const portadoraDeDoenca = false;
 const totalDeRendimentos = 3000000; //emCentavos
 
-//seu código aqui
+if(totalDeRendimentos>2855970){
+    if(aposentada||portadoraDeDoenca){
+        console.log("ISENTA!");
+    } else{
+        console.log("PEGA LEÃO!");
+    }
+} else{
+    console.log("VAZA LEÃO! JÁ TÁ DIFÍCIL SEM VOCÊ!");
+}
