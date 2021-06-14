578ea832617cbe45ce51a684aaf67bf7855af9e8
Fabiana Ferreira <fabiana.caroline13@gmail.com>
2021-06-14 19:16:44 -0300

    :tada: Exercício classe 4 - 14-06

---
+++ aula-14-06/back-integral-condicionais-main/classe-04/index.js
@@ -0,0 +1,7 @@
+const ladoA = 3;
+const ladoB = 3;
+let buchas = ["Branco", "Ás", "Duque", "Terno", "Quadra", "Quina", "Sena"];
+
+if(ladoB===ladoB){
+    console.log(`Sim! é uma bucha de ${buchas[ladoB]}`);
+}
