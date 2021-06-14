70a7058cbbf5610326b226dcf6e53d42b50bbc86
Fabiana Ferreira <fabiana.caroline13@gmail.com>
2021-06-14 18:38:58 -0300

    Exercício classe 01- 14-06

--- aula-14-06/back-integral-condicionais-main/classe-01/index.js
+++ aula-14-06/back-integral-condicionais-main/classe-01/index.js
@@ -1,4 +1,29 @@
 const jogada1 = "pedra"
-const jogada2 = "tesoura"
+const jogada2 = "pedra"
 
-//seu código aqui
+
+if(jogada1==="tesoura"){
+    if(jogada2==="pedra"){
+        console.log("pedra ✊");
+    } else if(jogada2==="papel"){
+        console.log("tesoura ✌️");
+    } else{
+        console.log("empate 🤡");
+    }
+} else if (jogada1==="pedra"){
+    if(jogada2==="tesoura"){
+        console.log("pedra ✊");
+    } else if(jogada2==="papel"){
+        console.log("papel 🖐️");
+    } else{
+        console.log("empate 🤡");
+    }
+} else{
+    if(jogada2==="pedra"){
+        console.log("papel 🖐️");
+    } else if(jogada2==="tesoura"){
+        console.log("tesoura ✌️");
+    } else{
+        console.log("empate 🤡");
+    }
+}
