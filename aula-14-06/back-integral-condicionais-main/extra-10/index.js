30ced963a2af83983f6a12b8c0660b4cb33deb10
Fabiana Ferreira <fabiana.caroline13@gmail.com>
2021-06-14 22:23:06 -0300

    :tada: Exercício 10 - 10-06

--- aula-14-06/back-integral-condicionais-main/extra-10/index.js
+++ aula-14-06/back-integral-condicionais-main/extra-10/index.js
@@ -1,1 +1,15 @@
 const caractere = "E";
+let vogal = ['a', 'e', 'i', 'o', 'u'];
+let vogalMaiuscula = ['A', 'E', 'I', 'O', 'U'];
+
+if((typeof caractere) == "string"){
+    if(vogal.includes(caractere)){
+        console.log("Vogal minúscula");
+    } else if( vogalMaiuscula.includes(caractere)){
+        console.log("Vogal maiúscula");
+    } else{
+        console.log("consoante");
+    }
+} else if((typeof caractere)== "number"){
+    console.log("Número");
+}
