170d66d616958039058ec38b88ef36f98b3185e6
Fabiana Ferreira <fabiana.caroline13@gmail.com>
2021-06-14 21:21:08 -0300

    :tada: Exercício 9 - 14-06

--- aula-14-06/back-integral-condicionais-main/extra-09/index.js
+++ aula-14-06/back-integral-condicionais-main/extra-09/index.js
@@ -1,1 +1,13 @@
 const nota = 8.5;
+
+if(nota>=9&&nota<=10){
+    console.log("conceito A");
+} else if(nota<=8.9&&nota>=8){
+    console.log("conceito B");
+} else if(nota<=7.9&&nota>=6){
+    console.log("conceito C");
+} else if(nota<=5.9&&nota>=4){
+    console.log("conceito D");
+} else {
+    console.log("conceito E");
+}
