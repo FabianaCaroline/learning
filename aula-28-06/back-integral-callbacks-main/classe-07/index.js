d680f7ac67542b3339be368d1ba3296643e958e2
Fabiana Ferreira <fabiana.caroline13@gmail.com>
2021-07-05 22:57:32 -0300

    :sparkles:Exercício classe 7 - 29-06

---
+++ aula-28-06/back-integral-callbacks-main/classe-07/index.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+const mensagem = "Estou aprendendo JavaScript na Cubos Academy";
+
+fs.writeFile("./meuarquivo.txt", mensagem, () => {
+    console.log("Arquivo criado com sucesso");
+})
