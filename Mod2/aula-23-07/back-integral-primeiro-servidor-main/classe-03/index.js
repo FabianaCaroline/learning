23c74e4cb88bc8b2e9c860717f265430616ec6c9
Fabiana Ferreira <fabiana.caroline13@gmail.com>
2021-07-23 22:16:57 -0300

    :sparkles: Servidor para controlar jogadas

--- Mod2/aula-23-07/back-integral-primeiro-servidor-main/classe-03/index.js
+++ Mod2/aula-23-07/back-integral-primeiro-servidor-main/classe-03/index.js
@@ -1,5 +1,17 @@
-
-
-
+const express = require('express');
+const app = express();
 
 const jogadores = ["José", "Maria", "João", "Marcos", "Fernanda"];
+cont = 0;
+
+app.get("/", (req, res) =>{
+    if (cont>=jogadores.length) {
+        cont = 0;
+    }
+    res.send(`É a vez de ${jogadores[cont]} jogar!`);
+
+    cont++;
+});
+
+app.listen(8000);
+
