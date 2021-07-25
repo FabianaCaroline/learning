bb25059e759474d6656c661d4c85d746942c5d78
Fabiana Ferreira <fabiana.caroline13@gmail.com>
2021-07-25 11:48:08 -0300

    :sparkles: Server controle de jogadores

--- Mod2/aula-23-07/back-integral-primeiro-servidor-main/extra-05/index.js
+++ Mod2/aula-23-07/back-integral-primeiro-servidor-main/extra-05/index.js
@@ -1,5 +1,49 @@
+const express = require('express');
+const app = express();
 
-
-
+const lodash = require('lodash');
 
 const jogadores = ["José", "Maria", "João", "Marcos", "Fernanda"];
+let cont = 0;
+
+app.get("/", (req, res) => {
+    if (cont >= jogadores.length) {
+        cont = 0;
+    }
+    res.send(`É a vez de ${jogadores[cont]} jogar!`);
+
+    cont++;
+});
+
+app.get("/remover", (req, res) => {
+    const indice = Number(req.query.indice);
+    if (jogadores[indice] === undefined) {
+        res.send(`Não existe jogador no índice informado (${indice}) para ser removido.`);
+    } else {
+        lodash.remove(jogadores, n => {
+            return n === jogadores[indice];
+        });
+        res.send(jogadores);
+    }
+});
+
+app.get("/adicionar", (req, res) => {
+    const nome = formatarNome(req.query.nome);
+    const indice = req.query.indice;
+    let resposta = jogadores;
+
+    if (indice === undefined) {
+        jogadores.push(nome);
+    } else if (jogadores[Number(indice)] == undefined) {
+        resposta = `O índice informado (${indice}) não existe no array. Novo jogador não adicionado.`;
+    } else {
+        jogadores.splice(indice, 0, nome);
+    }
+    res.send(resposta);
+});
+
+app.listen(8000);
+
+function formatarNome(nome) {
+    return nome[0].toUpperCase() + nome.substring(1).toLowerCase();
+}
