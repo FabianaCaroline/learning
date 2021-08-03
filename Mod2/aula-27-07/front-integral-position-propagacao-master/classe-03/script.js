6a348a891789d0bc01973718aab67d1f51053fee
Fabiana Ferreira <fabiana.caroline13@gmail.com>
2021-08-03 15:47:58 -0300

    :zap: Melhorando o código

--- Mod2/aula-27-07/front-integral-position-propagacao-master/classe-03/script.js
+++ Mod2/aula-27-07/front-integral-position-propagacao-master/classe-03/script.js
@@ -1,27 +1,18 @@
 const modal = document.querySelector('.modal');
-const imgBug1 = document.querySelector('.beetle');
-const imgBug2 = document.querySelector('.ladybug');
+const bugImg = document.querySelectorAll('.bug-img');
 
-
-imgBug1.addEventListener('click', (event) => {
-
-    modal.children[0].children[0].setAttribute('src', event.target.src);
-    modal.children[0].setAttribute('href', event.target.src);
-
-    modal.style.display = 'flex';
+bugImg.forEach(img => {
+    img.addEventListener('click', (event) => {
+    
+        modal.children[0].children[0].setAttribute('src', event.target.src);
+        modal.children[0].setAttribute('href', event.target.src);
+    
+        modal.style.display = 'flex';
+    });
 });
-imgBug2.addEventListener('click', (event) => {
-
-    modal.children[0].children[0].setAttribute('src', event.target.src);
-    modal.children[0].setAttribute('href', event.target.src);
-
-    modal.style.display = 'flex';
-});
-
 modal.children[0].addEventListener('click', (event) => {
     event.stopPropagation();
 });
-
 modal.addEventListener('click', (event) => {
     modal.style.display = 'none';
-})
+});
