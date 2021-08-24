baf5a14f9f63a89509ffe0a38142d660b40e5f9d
Fabiana Ferreira <fabiana.caroline13@gmail.com>
2021-08-24 19:15:08 -0300

    Verificando horário de funcionamento também no sábado

---
+++ Mod2/aula-02-08/back-integral-datas-em-js-main/classe-05/index.js
@@ -0,0 +1,27 @@
+function taAberto(date) {
+    const horas = date.getHours();
+    const minutos = date.getMinutes();
+    const diaSemana = date.getDay();
+
+
+    if (diaSemana !== 0 && diaSemana !== 6 && (horas >= 8 && horas <= 18)) {
+        if (horas === 18 && minutos > 0) {
+            return false;
+        } else {
+            return true;
+        }
+    } else if ((diaSemana === 6) && (horas >= 8 && horas <= 12)) {
+        if (horas === 12 && minutos > 0) {
+            return false;
+        } else {
+            return true;
+        }
+    } else {
+        return false;
+    }
+}
+
+console.log(taAberto(new Date(2021, 3, 25, 12)));
+console.log(taAberto(new Date(2021, 3, 26, 12)));
+console.log(taAberto(new Date(2021, 3, 26, 7, 59)));
+console.log(taAberto(new Date(2021, 3, 24, 9, 30)));
