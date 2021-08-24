49d069ae5151ebac5d858350196df0ee1b267a19
Fabiana Ferreira <fabiana.caroline13@gmail.com>
2021-08-24 19:57:47 -0300

    Formatando datas - idioma pt

--- Mod2/aula-02-08/back-integral-datas-em-js-main/extra-08/index.js
+++ Mod2/aula-02-08/back-integral-datas-em-js-main/extra-08/index.js
@@ -1,23 +1,23 @@
 const { format } = require("date-fns");
-const { default: ptBR } = require("date-fns/locale/pt-BR");
+const { pt } = require("date-fns/locale");
 
 function formatacao_a(date) {
-    return format(date, "dd 'de' MMMM 'de' yyyy",{ locale: ptBR});
+    return format(date, "dd 'de' MMMM 'de' yyyy",{ locale: pt});
 }
 function formatacao_b(date) {
-    return format(date, "dd/MM/yyyy",{ locale: ptBR});
+    return format(date, "dd/MM/yyyy",{ locale: pt});
 }
 function formatacao_c(date) {
-    return format(date, "d MMM",{ locale: ptBR});
+    return format(date, "d MMM",{ locale: pt});
 }
 function formatacao_d(date) {
-    return format(date, "dd MMM yyyy",{ locale: ptBR});
+    return format(date, "dd MMM yyyy",{ locale: pt});
 }
 function formatacao_e(date) {
-    return format(date, "dd 'de' MMM 'de' yyyy",{ locale: ptBR});
+    return format(date, "dd 'de' MMM 'de' yyyy",{ locale: pt});
 }
 function formatacao_f(date) {
-    return format(date, "dd/MMM",{ locale: ptBR});
+    return format(date, "dd/MMM",{ locale: pt});
 }
 
 const date = new Date();
