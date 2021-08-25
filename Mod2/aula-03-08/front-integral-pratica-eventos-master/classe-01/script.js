451ca16045c1f64c1340f5140a63561a24f42644
Fabiana Ferreira <fabiana.caroline13@gmail.com>
2021-08-25 12:29:13 -0300

    :tada: Galeria de fotos

---
+++ Mod2/aula-03-08/front-integral-pratica-eventos-master/classe-01/script.js
@@ -0,0 +1,62 @@
+const imagens = [{
+    src: "assets/gallery/image 1.png"
+},
+{
+   src:"assets/gallery/image 2.png"
+},
+{
+   src:"assets/gallery/image 3.png"
+},
+{
+   src:"assets/gallery/image 4.png"
+},
+{
+   src:"assets/gallery/image 5.png"
+},
+{
+   src:"assets/gallery/image 6.png"
+},
+{
+   src:"assets/gallery/image 7.png"
+},
+{
+   src:"assets/gallery/image 8.png"
+},
+{
+   src:"assets/gallery/image 9.png"
+},
+{
+   src:"assets/gallery/image 10.png"
+}];
+
+const galeria = document.querySelector(".galeria");
+
+document.body.onload = adicionarImagensGaleria;
+
+function adicionarImagensGaleria() {
+
+    imagens.forEach((x) => {
+        const cardImg = document.createElement("div");
+        cardImg.classList.add("card-img");
+
+        const img = document.createElement("img");
+        img.setAttribute("src", x.src);
+
+        const divDesc = document.createElement("div");
+        const h2 = document.createElement("h2");
+        const h3 = document.createElement("h3");
+        const textoH2 = document.createTextNode("Lorem ipsum");
+        const textoH3 = document.createTextNode("há 1 mês");
+
+        h2.appendChild(textoH2);
+        h3.appendChild(textoH3);
+
+        divDesc.appendChild(h2);
+        divDesc.appendChild(h3);
+
+        cardImg.appendChild(img);
+        cardImg.appendChild(divDesc);
+
+        galeria.appendChild(cardImg);
+    });
+}
